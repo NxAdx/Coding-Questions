789e1ef9cfa955a04eb9cd84cbb8e30c925879d1
“[NxAdx]” <[lokhandeaadarsh28@gmail.com]>
2024-01-17 14:49:47 +0530

    modify as per  numbering

--- pos_Nega_1.java
+++ pos_Nega_1.java
@@ -8,7 +8,7 @@
 
 import java.util.Scanner;
 
-class pos_Nega {
+class pos_Nega_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N;
--- sum_N_3.java
+++ sum_N_3.java
@@ -3,7 +3,7 @@
 
 import java.util.Scanner;
 
-public class sum_N {
+public class sum_N_3{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n, sum=0;
